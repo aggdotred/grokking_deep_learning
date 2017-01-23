2548342246993caf4796b14aaef7eb658d57eff4
Adam Gutierrez <hevyw8@gmail.com>
2017-01-22 21:57:37 -0600

    must i/o neural network

--- chapter-3/simple-neural-network.py
+++ chapter-3/simple-neural-network.py
@@ -7,12 +7,23 @@ toes = np.array([8.5, 9.5, 9.9, 9.0])
 wlrec = np.array([0.65, 0.8, 0.8, 0.9])
 nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
+#toes %win #fans
+ih_wgt = np.array([[0.1, 0.2, -0.1], #hid[0]
+                  [-0.1, 0.1, 0.9], #hid[1]
+                  [0.1, 0.4, 0.1]]).T #hid[2]
+
+# hid[0] hid[1] hid[2]
+hp_wgt = np.array([[0.3, 1.1, -0.3], #hurt?
+                  [0.1, 0.2, 0.0], #win?
+                  [0.0, 1.3, 0.1]]).T #sad?
+
 input = np.array([toes[0], wlrec[0], nfans[0]])
-weights = np.array([0.1, 0.2, 0])
+weights = [ih_wgt, hp_wgt]
 
 def neural_network(input, weights):
-    prediction = input.dot(weights)
-    return prediction
+    hid = input.dot(weights[0])
+    pred = hid.dot(weights[1])
+    return pred
 
 pred = neural_network(input, weights)
 print(pred)
